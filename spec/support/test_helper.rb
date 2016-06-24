9be185bbad97728b0647715a82a1e3c20f1c2e75
Nicolas Rodriguez <nrodriguez@jbox-web.com>
2016-06-24 04:42:09 +0200

    Add method to generate Demo menu in tests

--- spec/support/test_helper.rb
+++ spec/support/test_helper.rb
@@ -113,6 +113,26 @@ module TestHelper
     end
 
 
+    def demo_menu(primary)
+      primary.item :news, { icon: 'fa fa-fw fa-bullhorn', text: 'News' }, '/news_index_path'
+      primary.item :concerts, 'Concerts', '/concerts_path'
+      primary.item :video, 'Video', '/videos_path'
+      primary.item :divider_before_info, '', '#', html: { divider: true }
+      primary.item :info, { icon: 'fa fa-fw fa-book', title: 'Info' }, '/info_index_path', html: { split: true } do |info_nav|
+        info_nav.item :main_info_page, 'Main info page', '/main_info_page'
+        info_nav.item :about_info_page, 'About', '/about_info_page'
+        info_nav.item :misc_info_pages, 'Misc.', '#' do |misc_nav|
+          misc_nav.item :header_misc_pages, 'Misc. Pages', '#', html: { header: true }
+          misc_nav.item :page1, 'Page1', '/page1'
+          misc_nav.item :page2, 'Page2', '/page2'
+        end
+        info_nav.item :divider_before_contact_info_page, '', '#', html: { divider: true }
+        info_nav.item :contact_info_page, 'Contact', '/contact_info_page'
+      end
+      primary.item :signed_in, 'Signed in as Pavel Shpak', '#', html: { navbar_text: true }
+    end
+
+
     def empty_menu(primary)
     end
 
