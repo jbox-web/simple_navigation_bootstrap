baad2b32f65cbbddb6e641f02f544243ae0e22d3
Nicolas Rodriguez <nrodriguez@jbox-web.com>
2016-06-24 05:42:09 +0200

    Bump to version 1.0.1

--- lib/simple_navigation_bootstrap/version.rb
+++ lib/simple_navigation_bootstrap/version.rb
@@ -7,7 +7,7 @@ module SimpleNavigationBootstrap
   module VERSION
     MAJOR = 1
     MINOR = 0
-    TINY  = 0
+    TINY  = 1
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
