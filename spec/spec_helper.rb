5240d19f71ef44ce8f0ffe38173741909b8251c7
Nicolas Rodriguez <nicoladmin@free.fr>
2017-03-24 11:15:28 +0100

    Fix CodeClimate reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,8 @@
 require 'simplecov'
-require 'codeclimate-test-reporter'
 
 # Configure SimpleCov
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::HTMLFormatter,
-  CodeClimate::TestReporter::Formatter
 ])
 
 SimpleCov.start
