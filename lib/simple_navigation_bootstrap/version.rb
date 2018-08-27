aa0798acabe068d5275a2dec00ee104ac9a9db8e
Nicolas Rodriguez <nicoladmin@free.fr>
2018-08-27 20:18:35 +0200

    Bump to version 1.1.0

--- lib/simple_navigation_bootstrap/version.rb
+++ lib/simple_navigation_bootstrap/version.rb
@@ -8,8 +8,8 @@ module SimpleNavigationBootstrap
 
   module VERSION
     MAJOR = 1
-    MINOR = 0
-    TINY  = 1
+    MINOR = 1
+    TINY  = 0
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
