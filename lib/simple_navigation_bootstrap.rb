ff7cd3eaa607a4ee5a253ea48796a1834f0f8cdd
Nicolas Rodriguez <nicoladmin@free.fr>
2018-08-27 20:03:42 +0200

    Wrap requires in SimpleNavigationBootstrap module

--- lib/simple_navigation_bootstrap.rb
+++ lib/simple_navigation_bootstrap.rb
@@ -2,14 +2,14 @@
 
 require 'forwardable'
 require 'simple-navigation'
-require 'simple_navigation_bootstrap/errors'
-require 'simple_navigation_bootstrap/rendered_item'
-require 'simple_navigation_bootstrap/bootstrap_base'
-require 'simple_navigation_bootstrap/bootstrap2'
-require 'simple_navigation_bootstrap/bootstrap3'
-require 'simple_navigation_bootstrap/engine' if defined? Rails::Engine
 
 module SimpleNavigationBootstrap
+  require 'simple_navigation_bootstrap/errors'
+  require 'simple_navigation_bootstrap/rendered_item'
+  require 'simple_navigation_bootstrap/bootstrap_base'
+  require 'simple_navigation_bootstrap/bootstrap2'
+  require 'simple_navigation_bootstrap/bootstrap3'
+  require 'simple_navigation_bootstrap/engine' if defined? Rails::Engine
 end
 
 SimpleNavigation.register_renderer(bootstrap2: SimpleNavigationBootstrap::Bootstrap2)
