59e60abe04e5e75eb4e2e9a25474b12d3852f18d
Nicolas Rodriguez <nicoladmin@free.fr>
2018-12-19 04:50:58 +0100

    Add Bootstrap4 renderer

--- lib/simple_navigation_bootstrap.rb
+++ lib/simple_navigation_bootstrap.rb
@@ -9,8 +9,10 @@ module SimpleNavigationBootstrap
   require 'simple_navigation_bootstrap/bootstrap_base'
   require 'simple_navigation_bootstrap/bootstrap2'
   require 'simple_navigation_bootstrap/bootstrap3'
+  require 'simple_navigation_bootstrap/bootstrap4'
   require 'simple_navigation_bootstrap/engine' if defined? Rails::Engine
 end
 
 SimpleNavigation.register_renderer(bootstrap2: SimpleNavigationBootstrap::Bootstrap2)
 SimpleNavigation.register_renderer(bootstrap3: SimpleNavigationBootstrap::Bootstrap3)
+SimpleNavigation.register_renderer(bootstrap4: SimpleNavigationBootstrap::Bootstrap4)
---
+++ lib/simple_navigation_bootstrap/bootstrap4.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module SimpleNavigationBootstrap
+  class Bootstrap4 < SimpleNavigation::Renderer::Base
+
+    include BootstrapBase
+
+
+    private
+
+
+      def bootstrap_version
+        4
+      end
+
+
+      def navigation_class
+        'navbar-nav'
+      end
+
+
+      def container_class(level)
+        remove_navigation_class = options.fetch(:remove_navigation_class) { false }
+        remove_navigation_class ? '' : ['nav', navigation_class].compact
+      end
+
+  end
+end
