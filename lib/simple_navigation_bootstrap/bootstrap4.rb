1356fe9348b3645d288c93c1bf248070d5d038d4
Nicolas Rodriguez <nicoladmin@free.fr>
2019-02-09 19:22:29 +0100

    Coding style [ci skip]

--- lib/simple_navigation_bootstrap/bootstrap4.rb
+++ lib/simple_navigation_bootstrap/bootstrap4.rb
@@ -19,7 +19,7 @@ module SimpleNavigationBootstrap
       end
 
 
-      def container_class(level)
+      def container_class(_level)
         remove_navigation_class = options.fetch(:remove_navigation_class) { false }
         remove_navigation_class ? '' : ['nav', navigation_class].compact
       end
